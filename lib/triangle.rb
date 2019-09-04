fa8c2d5a9528aefa8ccd065c1ff7706c6be70e66
Marc Blanchard <marcblanchard0930@gmail.com>
2019-09-04 03:51:15 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,6 @@
 require 'pry'
 class Triangle
-  def initialize(equilateral, side_2, side_3)
+  def initialize(side_1, side_2, side_3)
     @triangle_sides = []
     @triangle_sides << side_1
     @triangle_sides << side_2
